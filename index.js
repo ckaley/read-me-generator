f5c8d5a60570e8c0121f904f7bcd13c2393bc080
Craig Kaley <craig@caucusroom.com>
2020-05-25 21:27:43 -0600

    Initial Checkin of solution.  Much of it already working.  Should have checked-in sooner

---
+++ index.js
@@ -0,0 +1,85 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const mdGen = require("./utils/generateMarkdown");
+
+const questions = [{
+  type: "input",
+  message: "Enter your Github username:",
+  name: "username"
+},
+{
+  type: "input",
+  message: "Enter your Project Title:",
+  name: "title"
+},
+{
+  type: "input",
+  message: "Enter your Project Description:",
+  name: "description"
+},
+{
+  type: "input",
+  message: "What are the steps required to install your project?",
+  name: "installation"
+},
+{
+  type: "input",
+  message: "Provide instructions and examples for use:",
+  name: "usage"
+},
+{
+  type: "list",
+  message: "What license does this project leverage?",
+  name: "license",
+  choices: [
+    "Apache",
+    "MIT",
+    "GNU",
+    "None"
+  ]
+},
+{
+  type: "input",
+  message: "Provide guidelines for contributing to this project:",
+  name: "contributing"
+},
+{
+  type: "input",
+  message: "Provide tests and guidelines to run them:",
+  name: "tests"
+},
+{
+  type: "input",
+  message: "Provide questions and answers that a user of this program may ask:",
+  name: "questions"
+}
+];
+
+
+inquirer
+  .prompt(questions)
+  .then(function (answers) {
+
+    const queryUrl = `https://api.github.com/users/${answers.username}`;
+
+    axios.get(queryUrl).then(function (res) {
+      let email;
+      if (res.data.email = "null") {
+        email = "No Email Address Provided on Profile"
+      } else {
+        email = res.data.email;
+      }
+      const picture = res.data.avatar_url;
+      console.log(email);
+      console.log(picture);
+      return answers;
+    }).then(function (answers) {
+
+      fs.writeFile("README.CAK", mdGen.generateMarkdown(answers), function (err) {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,48 @@
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+  
+## ${data.description}
+
+# Table of Contents
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [License](#License)
+* [Tests](#Tests)
+
+# Installation
+
+## ${data.installation}
+
+# Usage
+
+## ${data.usage}
+
+# License
+
+## ${data.license}
+
+# Contributing
+
+## ${data.contributing}
+
+* Tests
+
+## ${data.tests}
+
+* Questions
+
+## ${data.questions}
+
+
+`;
+}
+
+
+module.exports = {
+  generateMarkdown: generateMarkdown
+};
+
+
